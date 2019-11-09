7fcf02cb6a72ea1c8dccaa1a5fcb2a73f0578258
ianarriagamackenzie <iansamackenzie@gmail.com>
2019-11-09 08:23:56 -0700

    start of project

---
+++ eda/proposaleda.R
@@ -0,0 +1,46 @@
+## water quality proposal eda
+## ian arriaga mackenzie
+
+library(dplyr)
+library(tidyverse)
+library(mapdata)
+library(maps)
+library(stringr)
+library(viridis)
+library(ggplot2)
+library(ggmap)
+library(RColorBrewer)
+
+register_google(key = 'AIzaSyC0r9WOCNYUzgqgNAYHLQwKlhu8M5Kr_pE')
+
+wqdat = read.csv("C:/Users/iansa/OneDrive/Desktop/WaterAnalysisProject/data/waterquality2018update_csv.csv")
+toxdat = read.csv("C:/Users/iansa/OneDrive/Desktop/WaterAnalysisProject/data/tri_2017_co.csv")
+ahpdat = read.csv("C:/Users/iansa/OneDrive/Desktop/WaterAnalysisProject/data/AffordableHousingProjects_CARH_20180530 - AffordableHousingProjects_CARH_20180530.csv")
+
+toxdat2 = toxdat %>%
+  filter(X12..LATITUDE > min(wqdat$latitude)) %>% 
+  filter(X12..LATITUDE < max(wqdat$latitude)) %>% 
+  filter(X13..LONGITUDE > min(wqdat$longitude)) %>% 
+  filter(X13..LONGITUDE < max(wqdat$longitude))
+
+plot(wqdat$latitude, wqdat$longitude)
+plot(toxdat2$X12..LATITUDE, toxdat2$X13..LONGITUDE)
+
+states <- map_data("state")
+
+comap <- states %>%
+  filter(region %in% 'colorado')
+
+ahpdat2 = ahpdat %>% 
+  filter(city %in% 'Denver')
+ahpdat2$longitude = as.numeric(as.character(ahpdat2$longitude))
+ahpdat2$latitude = as.numeric(as.character(ahpdat2$latitude))
+
+get_googlemap(center = c(mean(wqdat$longitude), mean(wqdat$latitude)), zoom = 11) %>%
+  ggmap() +
+  geom_point(data = wqdat, mapping = aes(x = longitude, y = latitude),
+             color = brewer.pal(n = 8, name = 'Set2')[2]) +
+  geom_point(data = toxdat2, mapping = aes(x = X13..LONGITUDE, y = X12..LATITUDE),
+             color = brewer.pal(n = 8, name = 'Set2')[6]) + 
+  geom_point(data = ahpdat2, mapping = aes(x = longitude, y = latitude),
+             color = brewer.pal(n = 8, name = 'Set2')[3])
